ccc832f36985c4302a4e6f2cd416c52ddca1cec7
zhaojun <zhaojun@ucredit.com>
2016-08-18 10:23:49 +0800

    Initial Commit

---
+++ landScreen/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  landScreen
+//
+//  Created by jun zhao on 16/8/18.
+//  Copyright © 2016年 jun zhao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
